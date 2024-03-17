922934c4f19c2403f5d0e8be521a45fd0eaa8fd9
p43527170 <43527170@qq.com>
2024-03-17 23:25:27 +0800

    Initial commit

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Versions from './components/Versions.vue'
+
+const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+</script>
+
+<template>
+  <div class="actions">
+    <div class="action">
+      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
+    </div>
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+    </div>
+  </div>
+  <Versions />
+</template>
